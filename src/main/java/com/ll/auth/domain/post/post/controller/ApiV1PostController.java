0b46b8418ed1241f2aee4d4118dab6659c140d53
solji <solji.choi89@gmail.com>
2024-12-23 10:08:42 +0900

    authorId on write

--- src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
@@ -8,6 +8,7 @@ import com.ll.auth.domain.post.post.service.PostService;
 import com.ll.auth.global.rsData.RsData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.transaction.annotation.Transactional;
@@ -91,7 +92,9 @@ public class ApiV1PostController {
             String title,
             @NotBlank
             @Length(min = 2)
-            String content
+            String content,
+            @NotNull
+            Long authorId
     ) {
     }
 
